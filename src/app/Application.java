e9009f918fe46a309a9f10094d3ef07464accb41
mihajlovic-filip-emf <filip.mihajovic@studentfr.ch>
2025-02-10 19:26:29 +0100

    test non concluant

--- src/app/Application.java
+++ src/app/Application.java
@@ -1,17 +1,56 @@
 package app;
+import models.Personne;
 
 public class Application {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+        
+        public static void main(String[] args) {
+                Personne[] personnes = new Personne[100];
+                Personne personne1 = new Personne("Camille", "Onette");
+                Personne personne2 = new Personne("Alex", "Terrieur");
+                Personne personne3 = new Personne("Jaque", "Cellaire");
+                Personne personne4 = new Personne("Jean", "Bon");
+                Personne personne5 = new Personne("Pierre", "Caillou");
+                Personne personne6 = new Personne("Paul", "Emploi");
+                Personne personne7 = new Personne("Luka", "K");
+                Personne personne8 = new Personne("Filip", "Mihajlovic");
+                Personne personne9 = new Personne("Mihajlo", "Filipovic");
+                Personne personne10 = new Personne("Alex", "Da Silva");
+                Personne personne11 = new Personne("Thomas", "Mao");
+                Personne personne12 = new Personne("Ephram", "Guenaud");
+                personnes[0] = personne1;
+                personnes[1] = personne2;
+                personnes[2] = personne3;
+                personnes[3] = personne4;
+                personnes[4] = personne5;
+                personnes[5] = personne6;
+                personnes[6] = personne7;
+                personnes[7] = personne8;
+                personnes[8] = personne9;
+                personnes[9] = personne10;
+                personnes[10] = personne11;
+                personnes[11] = personne12;
+                personne1.ajouterUnAmi(personne11);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne12);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne3);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne4);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne5);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne6);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne7);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne8);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne9);
+                personne1.toString();
+                personne1.ajouterUnAmi(personne10);
+                personne1.toString();
+                personne1.supprimerUnAmi(personne11);
+                personne1.toString();
+        }
 
 }
--- src/models/Personne.java
+++ src/models/Personne.java
@@ -1,17 +1,51 @@
 package models;
-
 public class Personne {
-    
-        //
-        //              )        (                 ) (           (         (
-        //           ( /(   *   ))\ )        (  ( /( )\ )        )\ )  (   )\ )
-        //     (   ( )\())` )  /(()/((       )\ )\()|()/(  (    (()/(  )\ (()/(
-        //     )\  )((_)\  ( )(_))(_))\    (((_|(_)\ /(_)) )\    /(_)|((_) /(_))
-        //    ((_)((_)((_)(_(_()|_))((_)   )\___ ((_|_))_ ((_)  (_)) )\___(_))
-        //    __   _____ _____ ___ ___    ___ ___  ___  ___   ___ ___ ___   _
-        //    \ \ / / _ \_   _| _ \ __|  / __/ _ \|   \| __| |_ _/ __|_ _| | |
-        //     \ V / (_) || | |   / _|  | (_| (_) | |) | _|   | | (__ | |  |_|
-        //      \_/ \___/ |_| |_|_\___|  \___\___/|___/|___| |___\___|___| (_)
-        //
+        public final static int MAX_COPINS = 10;
+
+        String nom;
+        String prenom;
+        int nbAmis = 0;
+        Personne[] amis = new Personne[MAX_COPINS];
+        public Personne( String prenom, String nom) {
+            this.prenom = prenom;
+            this.nom = nom;
+        }
+
+        public String getNom() {
+            return this.nom;
+        }
+
+        public String getPrenom() {
+            return this.prenom;
+        }
+
+        public void ajouterUnAmi(Personne amisAjouter) {
+                if (this.nbAmis < MAX_COPINS) {
+                        for (int i = 0; i < amis.length; i++) {
+                                if (this.amis[i] == null) {
+                                    this.amis[i] = amisAjouter;
+                                    this.nbAmis++;
+                                    break;
+                                }
+                        }
+                } else {
+                    System.out.println("Vous avez atteint le nombre maximum d'amis");
+                        
+                }
+        }
+
+        public void supprimerUnAmi(Personne amisSupprimer) {
+                for (int i = 0; i < amis.length; i++) {
+                        if (this.amis[i] != null && this.amis[i].equals(amisSupprimer)) {
+                            this.amis[i] = null;
+                            this.nbAmis--;
+                            break;
+                        }
+                }
+        }
+
+        public void toSting() {
+            System.out.println(this.prenom + " " + this.nom.toUpperCase() + "et ses amis : ");
+        }
 
 }
